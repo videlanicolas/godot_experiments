629ca7b1312eb0a09db0181faf42e418aa898b66
Nicolás Videla <nvidela@google.com>
2024-07-13 08:36:37 +0000

    Camera movement update

--- CameraMovement/Camera3D.gd
+++ CameraMovement/Camera3D.gd
@@ -1,6 +1,7 @@
 extends Camera3D
 
 const FOLLOW_SPEED := 1.0
+const RAY_LENGTH = 1000
 
 @export
 var target : Node3D = null
@@ -14,23 +15,60 @@ var speedCurve : Curve
 @export
 var rotateSpeed := 1.0
 
+@export_range(0.0, 1.0)
+var slerpWeight := 0.0
+
+@export
+var withSlerp := false
+
+var selectedObject: CollisionObject3D = null
+
 func _physics_process(delta):
+	if Input.is_action_just_pressed("Select"):
+		var hitCollider := CheckClickRaycast()
+		if hitCollider:
+			selectedObject = hitCollider
+			target = hitCollider
+		else:
+			selectedObject = null
+			target = null
 	# If there is no target, skip doing anything.
 	if target == null:
 		pass
-	var n := target.global_position.normalized()
+	var n := global_position - target.global_position
 	# Check if the user pressed any input.
 	if Input.is_action_pressed("left"):
-		print(n)
-		global_rotate(n, delta * rotateSpeed)
+		global_position = n.rotated(Vector3.UP, -delta * rotateSpeed)
 	if Input.is_action_pressed("right"):
-		global_rotate(target.global_position.normalized(), -delta * rotateSpeed)
+		global_position = n.rotated(Vector3.UP, delta * rotateSpeed)
 	# Look at the target.
 	look_at(target.global_position)
 	# Now adjust the camera position according to the distance.
 	_adjust_distance_from_target(delta)
 
 func _adjust_distance_from_target(delta):
-	# First calculate the distance between the camera and the target.
+	# First calculate the vector the Camera should be in.
 	var desiredPosition := (global_position - target.global_position).normalized() * distance
+	# Interpolate the position of the camera to slowly adjust its position.
 	global_position = global_position.lerp(desiredPosition, delta * FOLLOW_SPEED)
+
+func CheckClickRaycast() -> CollisionObject3D:
+	# Get the physics space.
+	var space_state = get_world_3d().direct_space_state
+	# Get the mouse position.
+	var mousepos = get_viewport().get_mouse_position()
+	# Get the origin Vector3, this is the mouse position in the camera viewport
+	# converted to a Vector3 with origin in the global origin.
+	var origin = project_ray_origin(mousepos)
+	# The end Vector3 is the origin vector we defined before plus a vector that is
+	# the normal of the point in Vector3 coordinates of the mouse position, times the ray length.
+	var end = origin + project_ray_normal(mousepos) * RAY_LENGTH
+	# This gives us two vectors that we can easily calculate a ray.
+	# Create the query.
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	# Make the actualy query.
+	var result = space_state.intersect_ray(query)
+	if !result:
+		return null
+	print(result)
+	return result['collider']
