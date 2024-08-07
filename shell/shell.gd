f7fd3eeac3beb3f8885494d950c3ea010f11c3de
Nicolas Videla <videlanicolas@gmail.com>
2024-08-07 06:33:20 -0700

    Improving shell

--- shell/shell.gd
+++ shell/shell.gd
@@ -1,7 +1,8 @@
 class_name Shell extends LineEdit
 
 ## Emitted when the user presses enter, it communicates the command that was written by the user.
-signal command_submitted(cmd : String)
+## The command is splitted by empty space, and palced into an array.
+signal command_submitted(cmd : PackedStringArray)
 
 ## Symbol to display at the begining of the shell line.
 @export var shell_symbol : String
@@ -12,7 +13,8 @@ func _ready():
 func _on_text_submitted(new_text : String):
 	_reset()
 	if len(new_text) > (len(shell_symbol) + 1):
-		command_submitted.emit(new_text.substr(len(shell_symbol)))
+		var cmd := new_text.substr(len(shell_symbol))
+		command_submitted.emit(cmd.split(" ", false))
 
 func _on_text_changed(new_text : String):
 	if not new_text.begins_with(shell_symbol + " "):
